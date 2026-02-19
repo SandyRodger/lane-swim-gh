0dff2dc265fc144568de7316cd96bbacfd465beb
Sandy Rodger <abmrodger@gmail.com>
2026-02-19 11:46:12 +0000

    adding github link

--- src/components/timetable/pages/LaneSwimPage.jsx
+++ src/components/timetable/pages/LaneSwimPage.jsx
@@ -7,6 +7,16 @@ export default function LaneSwimPage() {
       <h1>Lane Swim</h1>
       <p>Prince Regent Swimming Complex — weekly lane swim timetable.</p>
       <p>Because the <a href="https://www.freedom-leisure.co.uk/centres/prince-regent-swimming-complex/timetables/">offical timetable</a> is very confusing</p>
+      <p>
+        <a
+          href="https://github.com/SandyRodger/lane-swim-gh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View source on GitHub
+        </a>
+      </p>
+
 
       <LaneSwimGrid
         schedule={laneSwimWeekly}
